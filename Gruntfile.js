dd16c7eefe06ae91b151c23e46de18b4197c066e
ran itzhaki <zamboney@gmail.com>
2015-02-10 15:34:48 +0200

    commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,113 +1,42 @@
 'use strict';
 module.exports = function(grunt) {
     require('load-grunt-tasks')(grunt)
+    var files = ["Gruntfile.js", "package.json", "dist/*.js", "coffee/*.coffee", "bower.json", "release.cmd", "commit.cmd"]
+    var message = "commit"
     grunt.initConfig({
         config: grunt.file.readJSON("bower.json"),
-        clean: {
-            build: ["lib/","dist/", "build/"],
-            postbuild: ["build/"]
-        },
-        bower: {
-            install: {
-                options: {
-                    layout: function(type, component, source) {
-                        var renamedType = type;
-                        if (source.indexOf("bundle") == -1 &&
-                            type == 'coffee' &&
-                            Object.getOwnPropertyNames(grunt.file.readJSON("bower.json").dependencies).indexOf(component) != -1)
-                            return "add";
-                        else
-                            return "remove";
-                    }
-                }
-            }
-        },
         version: {
             project: {
                 src: ['bower.json', 'package.json']
             }
         },
-        uglify: {
-            build: {
-                files: {
-                    'dist/<%= config.name %>.min.js': 'dist/<%= config.name %>.js'
-                }
-            },
-            bundle: {
-                files: {
-                    'dist/<%= config.name %>.bundle.min.js': 'dist/<%= config.name %>.bundle.js'
-                }
-            }
-        },
-        coffeescript_concat: {
-            bundle: {
-
-                files: {
-                    'coffee/<%= config.name %>.bundle.coffee': ['lib/add/*.coffee', 'coffee/*.coffee', '!coffee/*.bundle.coffee']
-                },
-
-            }
-        },
-        coffee: {
-            build: {
-                option: {
-                    join: true,
-                    extDot: 'last'
-                },
-                files: {
-                    'dist/<%= config.name %>.js': 'coffee/<%= config.name %>.coffee'
-                },
-
-            },
-            bundle: {
-                option: {
-                    join: true,
-                    extDot: 'last'
-                },
-                files: {
-                    'dist/<%= config.name %>.bundle.js': 'coffee/<%= config.name %>.bundle.coffee'
-                },
-
-            }
-        },
         gitcommit: {
-            build: {
+            all: {
+                options: {
+                    message: "<%= config.message %>",
+                    force: true
+                },
                 files: {
-                    src: ["dist/*.js", "coffee/*.coffee"]
+                    src: files
                 }
             },
             bower: {
                 options: {
+                    message: "release : <%= config.version %>",
                     force: true
                 },
                 files: {
-                    src: ["bower.json"]
+                    src: ["bower.json", "package.json"]
                 }
-            },
+            }
+        },
+        gitpush: {
             all: {
                 options: {
                     force: true
                 },
                 files: {
-                    src: ["Gruntfile.js", "package.json", "dist/*.js", "coffee/*.coffee", "bower.json"]
-                }
-            }
-        },
-        gitpush: {
-            firstTimer: {
-                options: {
-                    force: true
-                },
-                files: {
-                    src: ["Gruntfile.js", "package.json", "dist/*.js", "coffee/*.coffee", "bower.json", ".bowerrc"]
-                }
-            },
-            build: {
-                options: {
-                    force: true
-                },
-                files: {
-                    src: ["dist/*.js", "coffee/*.coffee"]
+                    src: files
                 }
             }
         },
@@ -117,7 +46,7 @@ module.exports = function(grunt) {
                     force: true
                 },
                 files: {
-                    src: ["Gruntfile.js", "package.json", "dist/*.js", "coffee/*.coffee", "bower.json", ".bowerrc"]
+                    src: files
                 }
             }
         },
@@ -127,23 +56,23 @@ module.exports = function(grunt) {
                     force: true
                 },
                 files: {
-                    src: ["dist/*.js", "coffee/*.coffee"]
+                    src: files
                 }
-            },
+            }
+        },
+        prompt: {
             all: {
                 options: {
-                    force: true
-                },
-                files: {
-                    src: ["Gruntfile.js", "package.json", "dist/*.js", "coffee/*.coffee", "bower.json", ".bowerrc"]
+                    questions: [{
+                        config: 'config.message',
+                        type: 'input',
+                        message: 'comment:\n',
+                        default: 'commit'
+                    }]
                 }
             }
         }
     })
-    grunt.registerTask('pull', ['gitpull:all']);
-    grunt.registerTask('build', ['clean:build', 'coffee:build', 'uglify:build', 'clean:postbuild']);
-    grunt.registerTask('bundle', ['clean:build', 'bower', 'coffeescript_concat', 'coffee', 'uglify', 'clean:postbuild']);
-    grunt.registerTask('commit', ['gitpull:build', 'gitcommit:build', 'gitpush:build']);
-    grunt.registerTask('firstTimer', ['gitadd', 'gitcommit:all', 'gitpush:firstTimer']);
+    grunt.registerTask('commit', ['prompt', 'gitadd', 'gitcommit:all', 'gitpush']);
     grunt.registerTask('release-git', ['version:project:patch', 'gitcommit:bower', 'release']);
 };
