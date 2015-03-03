4b89956db2d59a1f714932d18cc5de1fc1620104
carmeladi <carmeladi@gmail.com>
2015-03-03 11:10:53 +0200

    add classes to images

--- dist/sarine.viewer.image.bundle.js
+++ dist/sarine.viewer.image.bundle.js
@@ -1,6 +1,6 @@
 
 /*!
-sarine.viewer.image - v0.0.8 -  Monday, February 23rd, 2015, 2:15:30 PM 
+sarine.viewer.image - v0.0.8 -  Tuesday, March 3rd, 2015, 11:06:12 AM 
  The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
  */
 
@@ -90,12 +90,14 @@ sarine.viewer.image - v0.0.8 -  Monday, February 23rd, 2015, 2:15:30 PM
       for (index = _i = 0, _len = _ref.length; _i < _len; index = ++_i) {
         name = _ref[index];
         this.loadImage(this.src + name).then(function(img) {
-          var canvas, ctx;
+          var canvas, ctx, imgName;
           canvas = $("<canvas>");
           ctx = canvas[0].getContext('2d');
+          imgName = img.src.substr(img.src.lastIndexOf("/") + 1, img.src.lastIndexOf("/")).slice(0, -4);
           canvas.attr({
             width: img.width,
-            height: img.height
+            height: img.height,
+            "class": imgName
           });
           ctx.drawImage(img, 0, 0, img.width, img.height);
           _t.element.append(canvas);
