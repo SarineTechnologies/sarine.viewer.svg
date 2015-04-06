d97be1605aa4d4f9f04d461ee51ed4987f1f8b21
SarineColor <github@sarine.com>
2015-04-06 18:49:51 +0300

    deploy 0.1.0

--- dist/sarine.viewer.image.js
+++ dist/sarine.viewer.image.js
@@ -1,6 +1,6 @@
 
 /*!
-sarine.viewer.image - v0.0.10 -  Wednesday, March 25th, 2015, 6:31:24 PM 
+sarine.viewer.image - v0.1.0 -  Monday, April 6th, 2015, 6:48:37 PM 
  The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
  */
 
