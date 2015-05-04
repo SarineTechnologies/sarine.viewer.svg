f09e7186b976ac7003497cdcef407edf3bbe2681
ran itzhaki <zamboney@gmail.com>
2015-05-04 16:06:44 +0300

    change the position of the culet and from mm to precentages

--- dist/sarine.viewer.svg.js
+++ dist/sarine.viewer.svg.js
@@ -1,6 +1,6 @@
 
 /*!
-sarine.viewer.svg - v0.0.0 -  Tuesday, April 28th, 2015, 3:04:01 PM 
+sarine.viewer.svg - v0.1.1 -  Monday, May 4th, 2015, 4:01:26 PM 
  The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
  */
 
@@ -29,22 +29,23 @@ sarine.viewer.svg - v0.0.0 -  Tuesday, April 28th, 2015, 3:04:01 PM
       defer = $.Deferred();
       $.getJSON(this.src + this.jsonFileName, function(data) {
         _t.data = data;
-        return $(_t.element).load(_t.viewersBaseUrl + "atomic/" + _t.version + "/assets/" + _t.svg, function(data) {
-          _t.element.find("#SVG_width_mm").text(_t.data.Width.mm + "mm");
-          _t.element.find("#SVG_table_pre").text(_t.data["Table Size"].percentages + "%");
-          _t.element.find("#SVG_crown_pre").text(_t.data["Crown"]["height-percentages"] + "%");
-          _t.element.find("#SVG_crown_mm").text(_t.data["Crown"]["height-mm"] + "mm");
-          _t.element.find("#SVG_pavillion_pre").text(_t.data["Pavilion"]["height-percentages"] + "%");
-          _t.element.find("#SVG_pavillion_mm").text(_t.data["Pavilion"]["height-mm"] + "mm");
-          _t.element.find("#SVG_girdle_pre").text(_t.data["Girdle"]["Thickness-percentages"] + "%");
-          _t.element.find("#SVG_girdle_mm").text(_t.data["Girdle"]["Thickness-mm"] + "mm");
-          _t.element.find("#SVG_culet_mm").text(_t.data["Culet Size"].mm + "mm");
-          _t.element.find("#SVG_crown_rounded").text(_t.data["Crown"]["angel-deg"] + "°");
-          _t.element.find("#SVG_pavillion_rounded").text(_t.data["Pavilion"]["angel-deg"] + "°");
-          _t.element.find("#SVG_total_depth_per").text(_t.data["Total Depth"]["mm"] + "mm");
-          _t.element.find("#SVG_total_depth_mm").text(_t.data["Total Depth"]["percentages"] + "%");
-          return defer.resolve(_t);
+        $(_t.element).load(_t.viewersBaseUrl + "atomic/" + _t.version + "/assets/" + _t.svg, function(data) {
+          _t.element.find("#SVG_width_mm").text(parseFloat(_t.data.Width.mm).toFixed(2) + "mm");
+          _t.element.find("#SVG_table_pre").text(parseFloat(_t.data["Table Size"].percentages) + "%");
+          _t.element.find("#SVG_crown_pre").text(parseFloat(_t.data["Crown"]["height-percentages"]) + "%");
+          _t.element.find("#SVG_crown_mm").text(parseFloat(_t.data["Crown"]["height-mm"]).toFixed(2) + "mm");
+          _t.element.find("#SVG_pavillion_pre").text(parseFloat(_t.data["Pavilion"]["height-percentages"]) + "%");
+          _t.element.find("#SVG_pavillion_mm").text(parseFloat(_t.data["Pavilion"]["height-mm"]).toFixed(2) + "mm");
+          _t.element.find("#SVG_girdle_pre").text(parseFloat(_t.data["Girdle"]["Thickness-percentages"]) + "%");
+          _t.element.find("#SVG_girdle_mm").text(parseFloat(_t.data["Girdle"]["Thickness-mm"]).toFixed(2) + "mm");
+          _t.element.find("#SVG_culet_mm").text(parseFloat(_t.data["Culet Size"].percentages) + "%");
+          _t.element.find("#SVG_crown_rounded").text(parseFloat(_t.data["Crown"]["angel-deg"]).toFixed(2) + "°");
+          _t.element.find("#SVG_pavillion_rounded").text(parseFloat(_t.data["Pavilion"]["angel-deg"]).toFixed(2) + "°");
+          _t.element.find("#SVG_total_depth_per").text(parseFloat(_t.data["Total Depth"]["mm"]).toFixed(2) + "mm");
+          _t.element.find("#SVG_total_depth_mm").text(parseFloat(_t.data["Total Depth"]["percentages"]) + "%");
+          return defer.resolve(_t).fail(function() {});
         });
+        return defer.resolve(_t);
       });
       return defer;
     };
