93b9f6d20b390659c7ead3462289c898d20ca69a
SarineColor <github@sarine.com>
2015-05-26 11:12:12 +0300

    deploy 1.0.0

--- dist/sarine.viewer.svg.bundle.js
+++ dist/sarine.viewer.svg.bundle.js
@@ -1,6 +1,6 @@
 
 /*!
-sarine.viewer.svg - v0.1.2 -  Tuesday, May 12th, 2015, 11:18:33 AM 
+sarine.viewer.svg - v1.0.0 -  Tuesday, May 26th, 2015, 11:09:56 AM 
  The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
  */
 
