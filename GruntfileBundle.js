c204fe8bb1e255568d005e971c4c427691535e98
Lucky Luke <pelyem@gmail.com>
2017-04-25 16:45:56 +0300

    1. delete code that update banner in coffee file 2. delete code that produce coffee bundle 3. compilation only convert to js + bundle + grunt-contrib-uglify (=min + banner + remove comments + map) 4. delete dist from remote 5. have a buildFor environment variable to choose base viewer locally or from node_modules

--- GruntfileBundle.js
+++ GruntfileBundle.js
@@ -1,131 +1,57 @@
-'use strict';
+'use strict'
 module.exports = function(grunt) {
+
     require('load-grunt-tasks')(grunt)
-    var target = grunt.option('target') || "";
-    grunt
+    
+    function decideBaseViewer()
+    {
+        if(process.env.buildFor == 'deploy')
+        {
+            grunt.log.writeln("base viewer is in node_modules");
+
+            return 'node_modules/sarine.viewer/';
+        }
+        else
+        {
+            grunt.log.writeln("base viewer is locally relative to development environment");
+
+            return '../sarine.viewer/';
+        }
+    }
+
     grunt.initConfig({
-        config: grunt.file.readJSON(target + "bower.json"),
+        config: grunt.file.readJSON("package.json"),
         clean: {
-            build: [target + "lib/", target + "dist/", target + "build/"],
-            bundlecoffee: [target + "coffee/*.bundle.coffee"],
-            postbuild: [target + "build/"]
+            build: ["dist/"]
         },
-        commentsCoffee: {
-            coffee: {
-                src: [target + 'coffee/<%= config.name %>.coffee'],
-                dest: target + 'coffee/<%= config.name %>.coffee',
-            },
-            coffeeBundle: {
-                src: [target + 'coffee/<%= config.name %>.bundle.coffee'],
-                dest: target + 'coffee/<%= config.name %>.bundle.coffee',
-            },
-        },
-        concat: {
-            coffee: {
+        coffee: {
+            bundle: {
                 options: {
-                    stripBanners: true,
-                    banner: '###!\n<%= config.name %> - v<%= config.version %> - ' +
-                        ' <%= grunt.template.today("dddd, mmmm dS, yyyy, h:MM:ss TT") %> ' + '\n ' + grunt.file.read("copyright.txt") + '\n###',
+                    sourceMap: true,
                 },
-                src: [target + 'coffee/<%= config.name %>.coffee'],
-                dest: target + 'coffee/<%= config.name %>.coffee',
-            },
-            coffeebundle: {
-                options: {
-                    stripBanners: true,
-                    banner: '###!\n<%= config.name %> - v<%= config.version %> - ' +
-                        ' <%= grunt.template.today("dddd, mmmm dS, yyyy, h:MM:ss TT") %> ' + '\n ' + grunt.file.read("copyright.txt") + '\n###\n',
-                },
-                src: [target + 'coffee/<%= config.name %>.bundle.coffee'],
-                dest: target + 'coffee/<%= config.name %>.bundle.coffee',
-            }
-        },
-        bower: {
-            install: {
-                verbose : true,
-                bowerOptions: {
-                    forceLatest: true
-                },
-                options: {
-                    targetDir: target + "lib",
-                    layout: function(type, component, source) {
-                        var renamedType = type;
-                        if (source.indexOf("bundle") == -1 &&
-                            type == 'coffee' &&
-                            grunt.file.readJSON(target + "bower.json").dependencies &&
-                            Object.getOwnPropertyNames(grunt.file.readJSON(target + "bower.json").dependencies).indexOf(component) != -1)
-                            return "add";
-                        else
-                            return "remove";
-                    }
+                files: {
+                    'dist/<%= config.name %>.bundle.js' : [decideBaseViewer() + 'coffee/*.coffee', 'coffee/*.coffee'] // concat then compile into single file
                 }
             }
         },
         uglify: {
             options: {
-                preserveComments: 'some'
+                banner: '/*\n<%= config.name %> - v<%= config.version %> - ' +
+                        ' <%= grunt.template.today("dddd, mmmm dS, yyyy, h:MM:ss TT") %> ' + '\n ' + grunt.file.read("copyright.txt") + '\n*/',
+                preserveComments: false,
+                sourceMap : true,
+                sourceMapIn: "dist/<%= config.name %>.bundle.js.map"
             },
             build: {
-                src: target + 'dist/<%= config.name %>.js',
-                dest: target + 'dist/<%= config.name %>.min.js'
-            },
-            bundle: {
-                src: target + 'dist/<%= config.name %>.bundle.js',
-                dest: target + 'dist/<%= config.name %>.bundle.min.js'
-            }
-        },
-        coffeescript_concat: {
-            bundle: {
-                src: [target + 'lib/add/*.coffee', target + 'coffee/*.coffee', target + '!coffee/*.bundle.coffee'],
-                dest: target + 'coffee/<%= config.name %>.bundle.coffee'
-
-            }
-        },
-        coffee: {
-            build: {
-                option: {
-                    join: true,
-                    extDot: 'last'
-                },
-                dest: target + 'dist/<%= config.name %>.js',
-                src: [target + 'coffee/<%= config.name %>.coffee']
-
-            },
-            bundle: {
-                option: {
-                    join: true,
-                    extDot: 'last'
-                },
-                dest: target + 'dist/<%= config.name %>.bundle.js',
-                src: [target + 'coffee/<%= config.name %>.bundle.coffee']
-
+                src: 'dist/<%= config.name %>.bundle.js',
+                dest: 'dist/<%= config.name %>.bundle.min.js'
             }
         }
-    })
+    });
+    
     grunt.registerTask('bundle', [
         'clean:build',
-        'clean:bundlecoffee',
-        'bower',
-        'coffeescript_concat',
-        'commentsCoffee:coffeeBundle',
-        'concat:coffeebundle',
-        'coffee:bundle',
-        'commentsCoffee:coffee',
-        'concat:coffee',
-        'coffee:build',
-        'uglify',
-        'clean:postbuild'
+        'coffee',// Compile CoffeeScript files to JavaScript + concat + map
+        'uglify',//min + banner + remove comments + map    
     ]);
-    grunt.registerMultiTask('commentsCoffee', 'Remove comments from production code', function() {
-        this.files[0].src.forEach(function(file) {
-            var contents = grunt.file.read(file);
-            if (contents.match(/###!([\s\S]*?)###[\s\S]*?/gm))
-                contents = contents.replace(/###!([\s\S]*?)###[\s\S]*?/gm, "");
-            else {
-
-                contents = contents
-            }
-            grunt.file.write(file, contents);
-        });
-    });
 };
