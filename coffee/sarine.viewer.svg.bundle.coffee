c9ee2f2f14126e08d1b5be259c1aaa9bdf4e8e08
Alex Savenok <sawenok@gmail.com>
2017-10-18 15:25:01 +0300

    fixed no stone

--- coffee/sarine.viewer.svg.bundle.coffee
+++ coffee/sarine.viewer.svg.bundle.coffee
@@ -1,5 +1,5 @@
 ###!
-sarine.viewer.svg - v1.9.0 -  Wednesday, October 18th, 2017, 3:20:43 PM 
+sarine.viewer.svg - v1.9.0 -  Wednesday, October 18th, 2017, 3:24:41 PM 
  The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
 ###
 
@@ -52,7 +52,7 @@ class SarineSvg extends Viewer
 		
 		if !@src
 			@.failed().then( () -> 
-				defer.resolve(@)			
+				defer.resolve(_t)			
 			)		
 		else	
 			@fullSrc = if @src.indexOf('##FILE_NAME##') != -1 then @src.replace '##FILE_NAME##' , @jsonFileName else @src + @jsonFileName   			
--- dist/sarine.viewer.svg.js
+++ dist/sarine.viewer.svg.js
@@ -1,6 +1,6 @@
 
 /*!
-sarine.viewer.svg - v1.9.0 -  Wednesday, October 18th, 2017, 3:20:43 PM 
+sarine.viewer.svg - v1.9.0 -  Wednesday, October 18th, 2017, 3:24:41 PM 
  The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
  */
 
@@ -30,7 +30,7 @@ sarine.viewer.svg - v1.9.0 -  Wednesday, October 18th, 2017, 3:20:43 PM
       defer = $.Deferred();
       if (!this.src) {
         this.failed().then(function() {
-          return defer.resolve(this);
+          return defer.resolve(_t);
         });
       } else {
         this.fullSrc = this.src.indexOf('##FILE_NAME##') !== -1 ? this.src.replace('##FILE_NAME##', this.jsonFileName) : this.src + this.jsonFileName;
