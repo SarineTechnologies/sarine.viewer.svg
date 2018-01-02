5408a91c160768d20460484abf48d4b5738b5314
ronishoham <roni.shoham@sarine.com>
2018-01-02 16:34:11 +0200

    task 104352

--- GruntfileBundle.js
+++ GruntfileBundle.js
@@ -2,6 +2,9 @@
 module.exports = function(grunt) {
     require('load-grunt-tasks')(grunt)
     var target = grunt.option('target') || "";
+    var config = {};
+	config.dist = decideDist();
+
     grunt.initConfig({
         config: grunt.file.readJSON(target + "bower.json"),
         clean: {
@@ -104,6 +107,14 @@ module.exports = function(grunt) {
             bundle: {
                 dest: target + 'dist/<%= config.name %>.config',
                 src: [target + '<%= config.name %>.config']
+            },
+            dist_root_files: {
+                files: [{
+                        cwd: 'dist/',
+                        src: '**',
+                        dest: config.dist.root,
+                        expand: true
+                    }]
             }
         }
     })
@@ -121,7 +132,8 @@ module.exports = function(grunt) {
         'uglify',
         'clean:postbuild',
         'copyVersion',
-        'copy:bundle'
+        'copy:bundle',
+        'copy:dist_root_files'
     ]);
     grunt.registerMultiTask('commentsCoffee', 'Remove comments from production code', function() {
         this.files[0].src.forEach(function(file) {
@@ -148,4 +160,24 @@ module.exports = function(grunt) {
         copyFile.version = packageFile.version;
         grunt.file.write(configFileName , JSON.stringify(copyFile));
     });
+
+    function decideDist()
+    {
+        if(process.env.buildFor == 'deploy')
+        {
+            grunt.log.writeln("dist is github folder");
+
+            return {
+                root: 'app/dist/'
+            }
+        }
+        else
+        {
+            grunt.log.writeln("dist is local");
+
+            return {
+                root: '../../../dist/content/viewers/atomic/v1/js/'
+            }
+        }
+    }
 };
