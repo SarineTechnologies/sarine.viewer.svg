c0e3a54145088cf62861eb23566d0b3c9ec3bf46
ronishoham <roni.shoham@sarine.com>
2018-01-28 09:32:30 +0200

    done 107957

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,78 +1,188 @@
 'use strict';
 module.exports = function(grunt) {
     require('load-grunt-tasks')(grunt)
-    var files = ["Gruntfile.js", "copyright.txt", "GruntfileBundle.js", "package.json", "dist/*.js", "coffee/*.coffee", "bower.json", "release.cmd", "commit.cmd"]
-    var message = "commit"
+    var target = grunt.option('target') || "";
+    var config = {};
+	config.dist = decideDist();
+    config.coreFiles = getCoreFiles();
+    
     grunt.initConfig({
-        config: grunt.file.readJSON("bower.json"),
-        version: {
-            project: {
-                src: ['bower.json', 'package.json']
-            }
+        config: grunt.file.readJSON(target + "package.json"),
+        clean: {
+            build: [target + "lib/", target + "dist/", target + "build/"],
+            bundlecoffee: [target + "coffee/*.bundle.coffee"],
+            postbuild: [target + "build/"]
         },
-        gitcommit: {
-            all: {
-                options: {
-                    message: "<%= config.message %>",
-                    force: true
-                },
-                files: {
-                    src: files
-                }
+        commentsCoffee: {
+            coffee: {
+                src: [target + 'coffee/<%= config.name %>.coffee'],
+                dest: target + 'coffee/<%= config.name %>.coffee',
             },
-            bower: {
+            coffeeBundle: {
+                src: [target + 'coffee/<%= config.name %>.bundle.coffee'],
+                dest: target + 'coffee/<%= config.name %>.bundle.coffee',
+            },
+        },
+        concat: {
+            coffee: {
                 options: {
-                    message: "release : <%= config.version %>",
-                    force: true
+                    stripBanners: true,
+                    banner: '###!\n<%= config.name %> - v<%= config.version %> - ' +
+                        ' <%= grunt.template.today("dddd, mmmm dS, yyyy, h:MM:ss TT") %> ' + '\n ' + grunt.file.read("copyright.txt") + '\n###',
                 },
-                files: {
-                    src: ["bower.json", "package.json"]
-                }
-            }
-        },
-        gitpush: {
-            all: {
+                src: [target + 'coffee/<%= config.name %>.coffee'],
+                dest: target + 'coffee/<%= config.name %>.coffee',
+            },
+            coffeebundle: {
                 options: {
-                    force: true
+                    stripBanners: true,
+                    banner: '###!\n<%= config.name %> - v<%= config.version %> - ' +
+                        ' <%= grunt.template.today("dddd, mmmm dS, yyyy, h:MM:ss TT") %> ' + '\n ' + grunt.file.read("copyright.txt") + '\n###\n',
                 },
-                files: {
-                    src: files
-                }
+                src: [config.coreFiles , target + 'coffee/<%= config.name %>.bundle.coffee'],
+                dest: target + 'coffee/<%= config.name %>.bundle.coffee',
             }
         },
-        gitadd: {
-            firstTimer: {
-                option: {
-                    force: true
-                },
-                files: {
-                    src: files
-                }
-            }
-        },
-        gitpull: {
+        uglify: {
+            options: {
+                preserveComments: 'some'
+            },
             build: {
-                options: {
-                    force: true
-                },
-                files: {
-                    src: files
-                }
+                src: target + 'dist/<%= config.name %>.js',
+                dest: target + 'dist/<%= config.name %>.min.js'
+            },
+            bundle: {
+                src: target + 'dist/<%= config.name %>.bundle.js',
+                dest: target + 'dist/<%= config.name %>.bundle.min.js'
             }
         },
-        prompt: {
-            all: {
-                options: {
-                    questions: [{
-                        config: 'config.message',
-                        type: 'input',
-                        message: 'comment:\n',
-                        default: 'commit'
+        coffeescript_concat: {
+            bundle: {
+                src: [target + 'lib/add/*.coffee', target + 'coffee/*.coffee', target + '!coffee/*.bundle.coffee'],
+                dest: target + 'coffee/<%= config.name %>.bundle.coffee'
+
+            }
+        },
+        coffee: {
+            build: {
+                option: {
+                    join: true,
+                    extDot: 'last'
+                },
+                dest: target + 'dist/<%= config.name %>.js',
+                src: [target + 'coffee/<%= config.name %>.coffee']
+
+            },
+            bundle: {
+                option: {
+                    join: true,
+                    extDot: 'last'
+                },
+                dest: target + 'dist/<%= config.name %>.bundle.js',
+                src: [target + 'coffee/<%= config.name %>.bundle.coffee']
+
+            }
+        },
+        copy: {
+            bundle: {
+                dest: target + 'dist/<%= config.name %>.config',
+                src: [target + '<%= config.name %>.config']
+            },
+            dist_root_files: {
+                files: [{
+                        cwd: 'dist/',
+                        src: '**',
+                        dest: config.dist.root,
+                        expand: true
                     }]
-                }
             }
         }
     })
-    grunt.registerTask('commit', ['prompt', 'gitadd', 'gitcommit:all', 'gitpush']);
-    grunt.registerTask('release-git', ['release:' + grunt.file.readJSON("bower.json")["version"]]);
+    grunt.registerTask('build', [
+        'clean:build',
+        'clean:bundlecoffee',
+        'coffeescript_concat',
+        'commentsCoffee:coffeeBundle',
+        'concat:coffeebundle',
+        'coffee:bundle',
+        'commentsCoffee:coffee',
+        'concat:coffee',
+        'coffee:build',
+        'uglify',
+        'clean:postbuild',
+        'copyVersion',
+        'copy:bundle',
+        'copy:dist_root_files'
+    ]);
+    grunt.registerMultiTask('commentsCoffee', 'Remove comments from production code', function() {
+        this.files[0].src.forEach(function(file) {
+            var contents = grunt.file.read(file);
+            if (contents.match(/###!([\s\S]*?)###[\s\S]*?/gm))
+                contents = contents.replace(/###!([\s\S]*?)###[\s\S]*?/gm, "");
+            else {
+
+                contents = contents;
+            }
+            grunt.file.write(file, contents);
+        });
+    });
+       grunt.registerTask('copyVersion' , 'copy version from package.json to sarine.viewer.clarity.config' , function (){
+        var packageFile = grunt.file.readJSON(target + 'package.json');
+        var configFileName = target + packageFile.name + '.config';
+        var copyFile = null;
+        if (grunt.file.exists(configFileName))
+            copyFile = grunt.file.readJSON(configFileName);
+        
+        if (copyFile == null)
+            copyFile = {};
+
+        copyFile.version = packageFile.version;
+        grunt.file.write(configFileName , JSON.stringify(copyFile));
+    });
+
+    function decideDist()
+    {
+        if(process.env.buildFor == 'deploy')
+        {
+            grunt.log.writeln("dist is github folder");
+
+            return {
+                root: 'app/dist/'
+            }
+        }
+        else
+        {
+            grunt.log.writeln("dist is local");
+
+            return {
+                root: '../../../dist/content/viewers/atomic/v1/js/'
+            }
+        }
+    }
+
+    function getCoreFiles()
+    {
+        var core;
+
+        if(process.env.buildFor == 'deploy')
+        {
+            core = 
+            [
+                'node_modules/sarine.viewer/coffee/sarine.viewer.bundle.coffee'
+            ]
+
+            grunt.log.writeln("taking core files from node_modules");
+        }
+        else
+        {
+            core = 
+            [
+                '../../core/sarine.viewer/coffee/sarine.viewer.bundle.coffee'
+            ]
+
+            grunt.log.writeln("taking core files from parent folder");
+        }
+
+        return core;
+    }
 };
--- coffee/sarine.viewer.svg.bundle.coffee
+++ coffee/sarine.viewer.svg.bundle.coffee
@@ -1,5 +1,9 @@
 ###!
-sarine.viewer.svg - v1.9.0 -  Tuesday, January 2nd, 2018, 4:33:27 PM 
+sarine.viewer.svg - v1.9.0 -  Sunday, January 28th, 2018, 9:31:14 AM 
+ The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
+###
+###!
+sarine.viewer - v0.3.4 -  Wednesday, November 8th, 2017, 3:00:02 PM 
  The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
 ###
 
@@ -30,9 +34,40 @@ class Viewer
   convertElement : Error
   cancel : ()-> rm.cancel(@)
   loadImage : (src)-> rm.loadImage.apply(@,[src])
+  loadAssets : (resources, onScriptLoadEnd) ->
+    # resources item should contain 2 properties: element (script/css) and src.
+    if (resources isnt null and resources.length > 0)
+      scripts = []
+      for resource in resources
+          ###element = document.createElement(resource.element)
+          if(resource.element == 'script')
+            $(document.body).append(element)
+            # element.onload = element.onreadystatechange = ()-> triggerCallback(callback)
+            element.src = @resourcesPrefix + resource.src + cacheVersion
+            element.type= "text/javascript"###
+          if(resource.element == 'script')
+            scripts.push(resource.src + cacheVersion)
+          else
+            element = document.createElement(resource.element)
+            element.href = resource.src + cacheVersion
+            element.rel= "stylesheet"
+            element.type= "text/css"
+            $(document.head).prepend(element)
+      
+      scriptsLoaded = 0;
+      scripts.forEach((script) ->
+          $.getScript(script,  () ->
+              if(++scriptsLoaded == scripts.length) 
+                onScriptLoadEnd();
+          )
+        )
+
+    return      
   setTimeout : (delay,callback)-> rm.setTimeout.apply(@,[@delay,callback]) 
     
 @Viewer = Viewer 
+
+
 
 class SarineSvg extends Viewer
 	
